81ade1796a3ac6e5819722b3da6380c130b231cb
Adam <adam.gaoua@gmail.com>
2022-11-01 11:32:02 +0100

    fix heroku database

--- app/config/database.js
+++ app/config/database.js
@@ -1,11 +1,44 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+if (process.env.HEROKU_BASE_URL){
 
-const sequelize = new Sequelize("pokedex", "postgres", "adam", {
-    host: 'localhost',
-    dialect: 'postgres',
-    define: { timestamps: false }
-});
+    const db = new Sequelize(process.env.HEROKU_BASE_URL,{
+        dialect:"postgres",
+        dialectOptions: {
+            ssl : {
+                require:true,
+                rejectUnauthorized:false
+            }
+        }
 
+    } )
+    db
+    .authenticate()
+    .then(()=>{
+        console.log("connexion sequelize réussie :)")
+    })
+    .catch((error)=>{
+        console.error(error)
+    })
+    
+    module.exports = db;
+} else {
+
+    const sequelize = new Sequelize("pokedex", "postgres", "adam", {
+        host: 'localhost',
+        dialect: 'postgres',
+        define: { timestamps: false }
+    });
+    sequelize
+    .authenticate()
+    .then(()=>{
+        console.log("connexion sequelize réussie :)")
+    .catch((error)=>{
+        console.error(error)
+    })
+})
 module.exports = sequelize;
+}
+
+
